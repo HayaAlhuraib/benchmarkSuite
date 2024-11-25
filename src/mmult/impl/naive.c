b47443300a2f07b8421c09a0838faf258d514d7a
Haya Alhuraib <hayawi8002@gmail.com>
2024-11-25 18:35:09 +0300

    Update naive.c

--- src/mmult/impl/naive.c
+++ src/mmult/impl/naive.c
@@ -16,14 +16,14 @@ void* impl_scalar_naive(void* args) {
     args_t* arguments = (args_t*)args;
     size_t size = arguments->size;
 
-    float* A = (float*)arguments->input;
-    float* B = (float*)(arguments->input + size * size * sizeof(float));
+    float* A = (float*)arguments->input;                            // A starts at the beginning
+    float* B = (float*)(arguments->input + size * size * sizeof(float)); // B starts after A
     float* R = (float*)arguments->output;
 
     /* Perform matrix-matrix multiplication */
     for (size_t i = 0; i < size; i++) {
         for (size_t j = 0; j < size; j++) {
-            R[i * size + j] = 0.0f;
+            R[i * size + j] = 0.0f; // Initialize the result matrix
             for (size_t k = 0; k < size; k++) {
                 R[i * size + j] += A[i * size + k] * B[k * size + j];
             }
